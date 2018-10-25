20e17a4e10a79da97872319de0f64a8d996913cb
Aleksandr Tarasov <a.tarasov1209@gmail.com>
2018-10-25 20:48:07 +0100

    implemented function to get the mean RGB for each source image

---
+++ getMeanRGB.m
@@ -0,0 +1,17 @@
+function[redMean, greenMean, blueMean] = getMeanRGB(I)
+
+    [height, width, dim] = size(I);
+    imgArea = height * width;
+    [redMean, greenMean, blueMean] = deal(0);
+    for k=1:height
+        for j=1:width
+            redMean = redMean + double(I(k,j,1));
+            greenMean = greenMean + double(I(k,j,2));
+            blueMean = blueMean + double(I(k,j,3));
+        end
+    end
+    redMean = round(redMean/imgArea);
+    greenMean = round(greenMean/imgArea);
+    blueMean = round(blueMean/imgArea);
+end
+
---
+++ getMeanSource.m
@@ -0,0 +1,7 @@
+function[mean] = getMeanSource(sourceImages)
+    for k=1:length(sourceImages)
+        I = sourceImages{k};
+        [mean(k,1), mean(k,2), mean(k,3)] = getMeanRGB(I);
+    end
+    mean
+end
