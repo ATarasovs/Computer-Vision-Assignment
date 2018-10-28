d897c24cfc0bd836f9e4b0e9dd5e5d08b0db91a9
Aleksandr Tarasov <a.tarasov1209@gmail.com>
2018-10-28 19:08:52 +0000

    fixed function to get the number of tile across and down

--- getTileDimensions.m
+++ getTileDimensions.m
@@ -1,7 +1,7 @@
 function[tilesAcross, tilesDown] = getTileDimensions(I, tilesNum, tilesSize)
    [height, width, dim] = size(I);
-   tilesAcross = round(width/tilesSize);
-   tilesDown = round(height/tilesSize);
+   tilesAcross = floor(width/tilesSize);
+   tilesDown = floor(height/tilesSize);
 end
 
 
