0963ecc7a8dd6a67c490f0741af36bcddee5de7f
amarek1 <31806815+amarek1@users.noreply.github.com>
2018-11-13 22:50:13 +0000

    Add files via upload

--- amarek_functions/edge_det.m
+++ amarek_functions/edge_det.m
@@ -3,7 +3,7 @@
 %http://web.ipac.caltech.edu/staff/fmasci/home/astro_refs/HoughTrans_lines_09.pdf
 %https://uk.mathworks.com/help/images/hough-transform.html
 
-function [gray_pics] = edge_det(sourceImages)
+function [peaks] = edge_det(sourceImages)
 
 gray_pics=cell(1,length(sourceImages));
     for k = 1:length(sourceImages)
@@ -14,9 +14,23 @@ gray_pics=cell(1,length(sourceImages));
  edge_pics=cell(1,length(gray_pics));  
     for k = 1:length(gray_pics)
         I = gray_pics{k};
-        edge_pics{k} = edge(I,'canny',[0.0001 0.15]); %threshold->what is excluded
+        edge_pics{k} = edge(I,'canny',[0.2 0.3]); %threshold->what is excluded,[0.2 0.3]
     end
-    imshow(edge_pics{7})
+  
+     %H=double(1,length(gray_pics));  
+%     for k = 1:length(edge_pics)
+%         I = gray_pics{k};
+%         [H{k},theta{k},rho{k}] = hough(I);
+%     end
+    
+    for k = 1:length(edge_pics)
+        I = edge_pics{k};
+        [H{k},theta{k},rho{k}] = hough(I);
+        peaks(k) = length(houghpeaks(H{k},10000,'Threshold',1));
+    end
+    
 end
 
-  
+%peaks(k) = length(houghpeaks(H{k},10000,'Threshold',1)); 64%
+%peaks(k) = length(houghpeaks(H{k},10000,'Threshold',10)); 62%
+  %lines = houghlines(gray_pics{1},theta{1},rho{1},peaks,'FillGap',5,'MinLength',7)
