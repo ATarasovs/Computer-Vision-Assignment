d7216fb6df491581390b21e8cc501a10b1f33d1b
amarek1 <31806815+amarek1@users.noreply.github.com>
2018-11-19 01:31:11 +0000

    Add files via upload

--- amarek_functions/competition.m
+++ amarek_functions/competition.m
@@ -1,26 +1,30 @@
-tic
-imds_test=imageDatastore({'U:\CV assignment\Images\test_Sasha'});
-% imds_test.Labels=categorical(labels_test');
-imds_test.ReadSize=numpartitions(imds_test);
-imds_test.ReadFcn = @(loc)imresize(imread(loc),[50,50]);
+function[accuracy] = competition(trained_net, test_img_path)
+    tic
+    %Remove after
+    test_img_path2 = 'U:\CV assignment\Images\manmade_test';
+    imds_test=imageDatastore({test_img_path2,test_img_path});
+    % imds_test.Labels=categorical(labels_test');
+    imds_test.ReadSize=numpartitions(imds_test);
+    imds_test.ReadFcn = @(loc)imresize(imread(loc),[50,50]);
 
-%predict labels using previosuly trained convolutional neural network
-predict_labels = classify(trainedNet_90,imds_test);
+    %predict labels using previosuly trained convolutional neural network
+    predict_labels = classify(trained_net,imds_test);
 
-%get labels for test images for checking the accuracy
-% labels_test = getLabelsFromTextFile();
-% labels_double=cell2mat(labels_test(:,2));
-% labels_test_categorical=categorical(labels_double);
+    %get labels for test images for checking the accuracy
+    labels_test = getLabelsFromTextFile();
+    labels_double=cell2mat(labels_test(:,2));
+    labels_test_categorical=categorical(labels_double);
 
-%check accuracy
-accuracy = sum(predict_labels == labels_test_categorical)/numel(labels_test_categorical);
+    %check accuracy
+    accuracy = sum(predict_labels == labels_test_categorical)/numel(labels_test_categorical);
 
-toc
+    toc
 
-results = [labels_test, cellstr(predict_labels)];
+    %results = [labels_test, cellstr(predict_labels)];
 
-% labels_test0(1:250,1)=0;
-% labels_test0(251:500,1)=1;
+    % labels_test0(1:250,1)=0;
+    % labels_test0(251:500,1)=1;
+end
 
 
 % labels_test=labels_test'
