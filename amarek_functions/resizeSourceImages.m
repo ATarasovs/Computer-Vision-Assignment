765913039b88f01477aee4c4dcf530d9249ea14a
Aleksandr Tarasov <a.tarasov1209@gmail.com>
2018-11-22 18:43:56 +0000

    change the folder structure for classifier functions

--- amarek_functions/resizeSourceImages.m
+++
@@ -1,9 +0,0 @@
-
-function[resizedSourceImages] = resizeSourceImages(sourceImages)
-
-    for k = 1:length(sourceImages)
-        I = sourceImages{k};
-        resizedSourceImages{k} = imresize(I, [200 200]);
-    end
-    
-end
--- amarek_functions/sum_corners.m
+++
@@ -1,9 +0,0 @@
-function[nr_of_c] = sum_corners(corner_coordinates)
-
-    nr_of_c = length(corner_coordinates{1});
-    
-    for k = 2:length(corner_coordinates)
-       nr_of_c = nr_of_c + length(corner_coordinates{k});
-    end
-    
-end
